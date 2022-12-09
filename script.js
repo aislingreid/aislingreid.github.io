9075e24411dd5aa588fca0bbf3caa765c372c303
aislingreid <90197897+aislingreid@users.noreply.github.com>
2022-12-09 20:24:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import { WORDS } from "./words.js";
 
-function guessCheck(j) {
-  //check all boxes have been filled in
-  for (var i = 0; i < 5; i++) {
-  //loop through each box
-    if (GjLi === "") {
-      return false;
-    }
-    else {
-      return true;
-    }
+function validateGuess() {
+  //store the values from the form text fields into named variables
+  var guess = document.forms.Guess1.G1.value;
+ //if the variable contains an empty string show an alert in the browser:
+  if (guess === "") {
+    alert("Please enter your guess.");
+    return false;
+  //then check if the guess is the correct length
+    } else if (guess.length != 5) {
+         alert("Your guess must be 5 letters long");
+         return false; 
+    } 
+     else {
+    alert("A good guess!");
+    return true;
+   }  
 }
-
