98b321e4606f078b8caba88c5f36cb9476c05c3a
aislingreid <90197897+aislingreid@users.noreply.github.com>
2022-12-09 20:11:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function guessCheck() {
+import { WORDS } from "./words.js";
+
+function guessCheck(j) {
   //check all boxes have been filled in
-  var fullName = document.forms.Quiz.FullName.value;
-  var shortName = document.forms.Quiz.ShortName.value;
- //if either variable contains an empty string show an alert in the browser:
-  if (fullName === "") {
-    alert("Full name must be filled out");
-    return false;
-    } else if (shortName === "") {
-         alert("What shall we call you must be filled out");
-         return false; 
-    } 
-     else {
-    return true;
-   }  
+  for (var i = 0; i < 5; i++) {
+  //loop through each box
+    if (GjLi === "") {
+      return false;
+    }
+    else {
+      return true;
+    }
 }
+
