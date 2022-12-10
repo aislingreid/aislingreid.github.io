e66a9033f160ca81b3f9116620831933d1d97946
aislingreid <90197897+aislingreid@users.noreply.github.com>
2022-12-10 19:10:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function Test() {
   feedback.innerHTML = '<h2>Has it worked yet? </h2>';
   //alert("Testing...");
 }
+
+function Test2() {
+        let btn = document.getElementById('btnCount');
+        btn.addEventListener('click', () => {
+            let headings = document.getElementsByTagName('h1');
+            alert(`The number of H1 tags: ${headings.length}`);
+        });
+}
